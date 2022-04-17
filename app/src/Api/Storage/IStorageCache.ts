1815a433bc13f28c125c74c18df228d7226fa2a1
Lucas Fonte <lfonte007@gmail.com>
2022-04-17 09:35:43 -0300

    fix: remove unused files

--- app/src/Api/Storage/IStorageCache.ts
+++
@@ -1,7 +0,0 @@
-export interface IStorageCache<T> {
-  save(entity: T, expireSeconds?: number): Promise<T>;
-
-  findKey(key: string): Promise<T>;
-
-  deleteKey(key: string): Promise<void>;
-}
--- app/src/Api/Storage/IStorageRedis.ts
+++
@@ -1,7 +0,0 @@
-export interface IStorageRedis {
-  set(key: string, value: string, expireSeconds?: number): Promise<void>;
-
-  get(key: string): Promise<string>;
-
-  del(key: string): Promise<void>;
-}
